bba8b067dd0006bfff54bbf7573c8125ae567708
Iurii_Vinokurov <Iurii_Vinokurov@epam.com>
2020-08-09 19:36:54 +0300

    [task1]: csv_to_json_memory

---
+++ csv_to_json_memory.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const csv=require('csvtojson');
+
+const FROM = './csv/';
+const TO = './json/';
+const FILE_NAME = 'data';
+
+const convertData = async (from, to, fileName) => {
+  try {
+    const data = await csv().fromFile(`${from+fileName}.csv`);
+    await fs.promises.mkdir(to, {recursive: true});
+    await fs.promises.writeFile(`${to+fileName}.json`, JSON.stringify(data, null, 2));
+  } catch (e) {
+    console.error(e.message);
+  }
+
+};
+
+convertData(FROM, TO, FILE_NAME).then(r => console.log(r));
