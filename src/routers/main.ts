facf19d1d8db5b189513a37c90983c6d278021c9
Iurii_Vinokurov <Iurii_Vinokurov@epam.com>
2020-09-06 18:42:48 +0300

    [task2]: post

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import users from "../users.json";
+import { uuid } from 'uuidv4';
 
 const router = express.Router();
 const userList = users.users;
@@ -16,6 +17,28 @@ router.get('/', async (req, res, next) => {
 
   } else {
     await res.json(userList);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  const { login, password, age } = req.query;
+
+  if(userList.find(user => user.login === login)) {
+    await res.status(500).send(`there is a user with the login "${login}"`);
+    return null;
+  }
+
+  if (login && password) {
+    userList.push({
+      id: uuid(),
+      login: String(login),
+      password: String(password),
+      age: Number(age),
+      isDeleted: false,
+    });
+
+    console.log('userList', userList);
+    await res.status(200).send('ok');
   }
 });
 
